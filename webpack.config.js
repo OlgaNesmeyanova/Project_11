62e299c0b918ff3fad0487252c089c2ae0771964
Olga Nesmeyanova <olka445@yandex.com>
2020-09-28 15:55:45 +0300

    third commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,35 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
 module.exports = {
-    entry: { main: './src/JS/index.js'}
-}
+    entry: { main: './src/JS/index.js'},
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+            'file-loader',
+            {
+                loader: 'image-webpack-loader',
+                options: {
+                bypassOnDebug: true, 
+                disable: true, 
+                },
+            },
+            ],
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
+};
+const webpack = require('webpack');
+
+
+new webpack.DefinePlugin({
+    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+}) ;
